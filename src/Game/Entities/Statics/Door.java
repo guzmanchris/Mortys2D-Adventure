831706d2774deae4f9c507aadaafc0ae58ec4dc9
Joian Velez <josian.velez@upr.edu>
2017-02-02 16:51:25 -0400

    adds door that takes to menu

---
+++ src/Game/Entities/Statics/Door.java
@@ -0,0 +1,81 @@
+package Game.Entities.Statics;
+
+import Game.Entities.Creatures.Player;
+import Game.GameStates.MenuState;
+import Game.GameStates.State;
+import Main.Handler;
+import Resources.Images;
+import UI.UIManager;
+
+import java.awt.*;
+
+/**
+ * Created by Elemental on 2/2/2017.
+ */
+
+
+public class Door extends StaticEntity {
+
+    Rectangle ir = new Rectangle();
+    public Boolean EP = false;
+
+    public Door(Handler handler, float x, float y) {
+        super(handler, x, y, 64, 100);
+        health=10000000;
+        bounds.x=0;
+        bounds.y=0;
+        bounds.width = 100;
+        bounds.height = 64;
+
+        ir.width = bounds.width;
+        ir.height = bounds.height;
+        int irx=(int)(bounds.x-handler.getGameCamera().getxOffset()+x);
+        int iry= (int)(bounds.y-handler.getGameCamera().getyOffset()+height);
+        ir.y=iry;
+        ir.x=irx;
+    }
+
+    @Override
+    public void tick() {
+
+        if(isBeinghurt()){
+            setHealth(10000000);
+        }
+
+        if(handler.getKeyManager().attbut){
+            EP=true;
+
+        }else if(!handler.getKeyManager().attbut){
+            EP=false;
+        }
+
+    }
+
+    @Override
+    public void render(Graphics g) {
+        g.drawImage(Images.door,(int)(x-handler.getGameCamera().getxOffset()),(int)(y-handler.getGameCamera().getyOffset()),width,height,null);
+
+        g.setColor(Color.black);
+        checkForPlayer(g, handler.getWorld().getEntityManager().getPlayer());
+    }
+
+    private void checkForPlayer(Graphics g, Player p) {
+        Rectangle pr = p.getCollisionBounds(0,0);
+
+        if(ir.contains(pr) && !EP){
+            g.drawImage(Images.E,(int) x+width,(int) y+10,32,32,null);
+        }else if(ir.contains(pr) && EP){
+            g.drawImage(Images.EP,(int) x+width,(int) y+10,32,32,null);
+            State.setState(handler.getGame().menuState);
+
+
+        }
+
+
+    }
+
+    @Override
+    public void die() {
+
+    }
+}
