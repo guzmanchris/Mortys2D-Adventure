247be5208158da6a8198b2488d3e9aa0b9084be1
Joshua <Joshua@169.254.243.226>
2018-10-25 11:26:56 -0400

    Made Door Invisible

--- src/Game/Entities/Statics/Door.java
+++ src/Game/Entities/Statics/Door.java
@@ -17,69 +17,76 @@ import com.sun.glass.events.KeyEvent;
 
 public class Door extends StaticEntity {
 
-    private Rectangle ir = new Rectangle();
-    public Boolean EP = false;
+	private Rectangle ir = new Rectangle();
+	public Boolean EP = false;
 
-    private BaseWorld world;
+	private BaseWorld world;
 
-    public Door(Handler handler, float x, float y,BaseWorld world) {
-        super(handler, x, y, 64, 100);
-        this.world=world;
-        health=10000000;
-        bounds.x=0;
-        bounds.y=0;
-        bounds.width = 100;
-        bounds.height = 64;
+	public Door(Handler handler, float x, float y,BaseWorld world) {
+		super(handler, x, y, 64, 100);
+		this.world=world;
+		health=10000000;
+		bounds.x=0;
+		bounds.y=0;
+		bounds.width = 100;
+		bounds.height = 64;
+		visible = false;
 
-        ir.width = bounds.width;
-        ir.height = bounds.height;
-        int irx=(int)(bounds.x-handler.getGameCamera().getxOffset()+x);
-        int iry= (int)(bounds.y-handler.getGameCamera().getyOffset()+height);
-        ir.y=iry;
-        ir.x=irx;
-    }
+		ir.width = bounds.width;
+		ir.height = bounds.height;
+		int irx=(int)(bounds.x-handler.getGameCamera().getxOffset()+x);
+		int iry= (int)(bounds.y-handler.getGameCamera().getyOffset()+height);
+		ir.y=iry;
+		ir.x=irx;
+	}
 
-    @Override
-    public void tick() {
+	@Override
+	public void tick() {
+		if (visible==true){
 
-        if(isBeinghurt()){
-            setHealth(10000000);
-        }
+			if(isBeinghurt()){
+				setHealth(10000000);
+			}
 
-        if(handler.getKeyManager().attbut){
-            EP=true;
+			if(handler.getKeyManager().attbut){
+				EP=true;
 
-        }else if(!handler.getKeyManager().attbut){
-            EP=false;
-        }
+			}else if(!handler.getKeyManager().attbut){
+				EP=false;
+			}
+		}
+		else;
 
-    }
+	}
 
-    @Override
-    public void render(Graphics g) {
-        g.drawImage(Images.door,(int)(x-handler.getGameCamera().getxOffset()),(int)(y-handler.getGameCamera().getyOffset()),width,height,null);
+	@Override
+	public void render(Graphics g) {
+		if(visible ==true){
+			g.drawImage(Images.door,(int)(x-handler.getGameCamera().getxOffset()),(int)(y-handler.getGameCamera().getyOffset()),width,height,null);
 
-        g.setColor(Color.black);
-        checkForPlayer(g, handler.getWorld().getEntityManager().getPlayer());
-    }
+			g.setColor(Color.black);
+			checkForPlayer(g, handler.getWorld().getEntityManager().getPlayer());
+		}else;
+	}
 
-    private void checkForPlayer(Graphics g, Player p) {
-        Rectangle pr = p.getCollisionBounds(0,0);
+	private void checkForPlayer(Graphics g, Player p) {
+		Rectangle pr = p.getCollisionBounds(0,0);
 
-        if(ir.contains(pr) && !EP){
-            g.drawImage(Images.E,(int) x+width,(int) y+10,32,32,null);
-        }else if(ir.contains(pr) && EP  || handler.getKeyManager().keyJustPressed(KeyEvent.VK_TAB)){
-            g.drawImage(Images.EP,(int) x+width,(int) y+10,32,32,null);
-            g.drawImage(Images.loading,0,0,800,600,null);
-            handler.setWorld(world);
+		if(ir.contains(pr) && !EP){
+			g.drawImage(Images.E,(int) x+width,(int) y+10,32,32,null);
+		}else if(ir.contains(pr) && EP  || handler.getKeyManager().keyJustPressed(KeyEvent.VK_TAB)){
+			g.drawImage(Images.EP,(int) x+width,(int) y+10,32,32,null);
+			g.drawImage(Images.loading,0,0,800,600,null);
+			handler.setWorld(world);
 
-        }
+		}
 
 
-    }
 
-    @Override
-    public void die() {
+	}
 
-    }
+	@Override
+	public void die() {
+
+	}
 }
